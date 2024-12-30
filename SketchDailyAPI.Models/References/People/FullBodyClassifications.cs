9c7695a37b764f7ee979afed7ac3505da041d9f9
faintpixel <artomizer@sketchdaily.net>
2024-12-30 09:26:50 -0700

    fix for full body upload

--- SketchDailyAPI.Models/References/People/FullBodyClassifications.cs
+++ SketchDailyAPI.Models/References/People/FullBodyClassifications.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SketchDailyAPI.Models.References.People
 {
     public class FullBodyClassifications : BaseClassifications
     {
+        [JsonConverter(typeof(StringToNullableBoolConverter))]
         public bool? NSFW { get; set; }
+
+        [JsonConverter(typeof(StringToNullableBoolConverter))]
         public bool? Clothing { get; set; }
+
         public Gender? Gender { get; set; }
         public PoseType? PoseType { get; set; }
         public ViewAngle? ViewAngle { get; set; }
---
+++ SketchDailyAPI.Models/StringToNullableBoolConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class StringToNullableBoolConverter : JsonConverter<bool?>
+{
+    public override bool? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var stringValue = reader.GetString();
+            if (bool.TryParse(stringValue, out var result))
+            {
+                return result;
+            }
+        }
+        else if (reader.TokenType == JsonTokenType.True)
+        {
+            return true;
+        }
+        else if (reader.TokenType == JsonTokenType.False)
+        {
+            return false;
+        }
+        return null;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool? value, JsonSerializerOptions options)
+    {
+        if (value.HasValue)
+        {
+            writer.WriteBooleanValue(value.Value);
+        }
+        else
+        {
+            writer.WriteNullValue();
+        }
+    }
+}
